5b72dbee77a2529e311b3ef7e74c834a3419d4e2
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-10-18 23:28:44 +0530

    added register

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import React, { Component } from 'react';
 import Register from './views/user/register';
 
 class App extends Component {
-  name = 'Foodie';
+  // name = 'Foodie';
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <p>
-            Hello {this.name}
+          <Register/>
           </p>
         </header>
       </div>
