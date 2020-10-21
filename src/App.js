04640541782e63030f049884c45333f6bff7f6af
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-10-21 23:08:10 +0530

    Updated signin and signup route

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
-import './App.css';
-import React, { Component } from 'react';
-import Register from './views/user/register';
+import "./App.css";
+import React, { Component } from "react";
+import { Switch, Route, BrowserRouter } from "react-router-dom";
+import Register from "./views/user/Register";
+import Login from "./views/user/Login";
+import { Home } from './views/Home';
 
 class App extends Component {
-  // name = 'Foodie';
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-          <Register/>
-          </p>
-        </header>
+      <div className="app-routes">
+        <BrowserRouter>
+          <Switch>
+            <Route path="/signin" component={Login} />
+            <Route path="/signup" component={Register} />
+            <Route path="/" component={Home} />
+
+          </Switch>
+        </BrowserRouter>
       </div>
     );
   }
