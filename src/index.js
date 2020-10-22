b1183c5d17b718fb805897da8e97bcd1ecd2ac94
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-10-22 23:58:43 +0530

    updated index js and css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import './index.css';
-import App from './App';
+import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
 // import 'font-awesome/css/font-awesome.min.css';
 
