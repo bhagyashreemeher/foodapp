f2b3cb94eb0e056c26c0e84165559c6d13fb4423
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-11-05 23:34:32 +0530

    Added header in home component

---
+++ src/views/Header.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import "../App.css"
+
+export const Header = (props) => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-light" style={{background: "linear-gradient(to right, #9c27b0, #e040fb)"}}>
+            <a className="navbar-brand" href="#">Navbar</a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    </li>
+                    {/* <li className="nav-item">
+                        <a className="nav-link" href="#">Features</a>
+                    </li> */}
+                </ul>
+            </div>
+        </nav>
+    )
+}
