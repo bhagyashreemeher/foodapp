7a7152f905815c7fad14f23967d9bfc7c9deae08
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-11-07 23:52:58 +0530

    Added Article component

---
+++ src/views/Articles.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+
+export const Articles = () => {
+    return (
+        <div class="card-deck">
+            <div class="card border-light text-white bg-primary">
+                {/* <img src="..." class="card-img-top" alt="..." /> */}
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                </div>
+                <div class="card-footer bg-info">
+                    <small class="text-white">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card border-light text-white bg-primary">
+                {/* <img src="..." class="card-img-top" alt="..." /> */}
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+                </div>
+                <div class="card-footer bg-info">
+                    <small class="text-white">Last updated 3 mins ago</small>
+                </div>
+            </div>
+            <div class="card border-light text-white bg-primary">
+                {/* <img src="..." class="card-img-top" alt="..." /> */}
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                </div>
+                <div class="card-footer bg-info">
+                    <small class="text-white">Last updated 3 mins ago</small>
+                </div>
+            </div>
+        </div>
+    )
+}
