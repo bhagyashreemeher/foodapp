c8e90d88125dd84ba66638cae6678662b299e46d
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-11-08 23:34:58 +0530

    added font-awesome

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'font-awesome/css/font-awesome.min.css';
 
 import './index.css';
+import 'font-awesome/css/font-awesome.min.css';
+
 import App from './App.jsx';
 import * as serviceWorker from './serviceWorker';
-// import 'font-awesome/css/font-awesome.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
