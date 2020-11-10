d2f799f90a9991f1cc872cb3113b1233c255f13f
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-11-10 21:03:59 +0530

    updated home component

--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 import { Header } from './Header'
+import { Articles } from './Articles'
 
 export const Home = (props) => {
     return (
-        <Header/>
+        <div>
+            <Header />
+            <div style={{ padding: "10px 30px 0 30px" }}>
+                <nav className="nav mb-3">
+                    {/* <i className="fa fa-plus"></i> */}
+                    <a className="nav-link" href="/" style={{ color: "#007bff" }}>
+                        <i className="fa fa-lg fa-plus" style={{ color: "#007bff" }}></i> Article
+                    </a>
+                    {/* <a className="nav-link active" href="/">New Article</a> */}
+                    {/* <a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a> */}
+                </nav>
+                <Articles />
+            </div>
+        </div>
     )
 }
