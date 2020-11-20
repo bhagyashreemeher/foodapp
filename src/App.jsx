e7703a1b09af68af0a0bcdbc30875bd860834c95
Bhagyashree Meher <bhagya.meher@gmail.com>
2020-11-20 21:41:42 +0530

    fixed the import

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import React, { Component } from "react";
 import { Switch, Route, BrowserRouter, Redirect } from "react-router-dom";
-import Register from "./views/user/Register.jsx";
-import Login from "./views/user/Login.jsx";
+import Register from "./views/User/Register.jsx";
+import Login from "./views/User/Login.jsx";
 import { Home } from './views/Home.jsx';
 import { isAuthenticated } from './constant';
 
